f90415a1adebea067c276869295f207798782f2c
googlymonster <googlycodes@gmail.com>
2020-09-20 14:25:52 -0500

    updates

--- covid19tracker/src/components/Cards/Cards.jsx
+++ covid19tracker/src/components/Cards/Cards.jsx
@@ -3,6 +3,7 @@ import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 import CountUp from "react-countup";
 import cx from "classnames";
+
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   if (!confirmed) {
     return "Loading ...";
@@ -88,6 +89,33 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
             </Typography>
             <Typography variant="body2">
               Number of deaths by COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={3}
+          component={Card}
+          className={cx(styles.card, styles.percents)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Mortality Rate
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={(deaths.value/confirmed.value)*100}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              Percent of deaths by COVID-19
             </Typography>
           </CardContent>
         </Grid>
